afabd04ac4a2e539974c10fae437853fd3f075e0
Chenghz <chz2001@126.com>
2025-09-04 18:12:23 +0800

    VERSION: 1.3.8

--- src/index.ts
+++ src/index.ts
@@ -35,7 +35,7 @@ class ModaoProtoMcpServer {
     this.server = new Server(
       {
         name: 'modao-proto-mcp',
-        version: '1.3.7',
+        version: '1.3.8',
       },
       {
         capabilities: {
